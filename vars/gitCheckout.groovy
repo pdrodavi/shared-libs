9872f179b36fad53663e83c1ace3897c1966442d
Pedro Davi <pedrodaviti@gmail.com>
2023-02-17 19:49:51 -0300

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -8,8 +8,8 @@ def call(){
     ])
 
     echo "Branch selecionada: ${inputBranch}"
-    echo "Clonando repositório: https://gitlab.com/srvextechnology/app-job-deploy-push-registry.git"
+    echo "Clonando repositório: https://gitlab.com/pedrodavi/app-job-deploy-sb.git"
 
-    git(branch: "${inputBranch}", credentialsId: 'gitlab-token', url: "https://gitlab.com/srvextechnology/app-job-deploy-push-registry.git")
+    git(branch: "${inputBranch}", credentialsId: 'gitlab-token', url: "https://gitlab.com/pedrodavi/app-job-deploy-sb.git")
 
 }
