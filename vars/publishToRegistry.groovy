1361c5a75c4e4b5b45051a4ce6b3b531b5b9f20c
Pedro Davi <pedrodaviti@gmail.com>
2023-02-17 21:11:45 -0300

    Update publishToRegistry.groovy

--- vars/publishToRegistry.groovy
+++ vars/publishToRegistry.groovy
@@ -1,6 +1,19 @@
 def call() {
-  withDockerRegistry(credentialsId: 'JFROG-PLATAFORM', url: 'https://srvextechnology.jfrog.io/artifactory/registry-docker/') {
-    sh "docker tag ${env.JOB_BASE_NAME} srvextechnology.jfrog.io/registry-docker/${env.JOB_BASE_NAME}:latest"
-    sh "docker push srvextechnology.jfrog.io/registry-docker/${env.JOB_BASE_NAME}:latest"
+  
+  inputPublish = input([
+    message: 'Publish to Registry?',
+    parameters: [
+      choice(name: 'Publish', choices: ['Yes', 'No'], description: 'Publish image to artifactory')
+    ]
+  ])
+  
+  if ("${inputPublish}" == 'Yes') {
+      withDockerRegistry(credentialsId: 'JFROG-PLATAFORM', url: 'https://srvextechnology.jfrog.io/artifactory/registry-docker/') {
+        sh "docker tag ${env.JOB_BASE_NAME} srvextechnology.jfrog.io/registry-docker/${env.JOB_BASE_NAME}:latest"
+        sh "docker push srvextechnology.jfrog.io/registry-docker/${env.JOB_BASE_NAME}:latest"
+    }
+  } else {
+    echo 'Step Skipped'
   }
+  
 }
