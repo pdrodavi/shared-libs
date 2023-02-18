8eeb63c5b7e412188caa4b97b68d589fd0d999e0
Pedro Davi <pedrodaviti@gmail.com>
2023-02-17 21:59:10 -0300

    Update startContainer.groovy

--- vars/startContainer.groovy
+++ vars/startContainer.groovy
@@ -1,13 +1,4 @@
 def call() {
-  try {
-    sh "docker container stop ${env.JOB_BASE_NAME}"
-    sh "docker container rm -f ${env.JOB_BASE_NAME}"
-  } catch (err) {
-    echo err.getMessage()
-  }
-
-  sh "docker container create --name=${env.JOB_BASE_NAME} --memory=256MB --memory-reservation=256MB --network=bridge --env-file=${env.WORKSPACE}/.env --restart=always --publish-all ${env.JOB_BASE_NAME}:latest"
-
-  sh "docker container start ${env.JOB_BASE_NAME}"
+  sh "docker run -i --rm --memory=256MB --memory-reservation=256MB --network=bridge --restart=always --publish-all ${env.JOB_BASE_NAME}"
   cleanWs()
 }
